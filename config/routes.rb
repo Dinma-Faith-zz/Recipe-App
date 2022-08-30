298e00a49cc834a712a832614301ccfff393dbb2
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-30 22:58:40 +0100

    Implementation of public recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'public_recipes/index'
   root 'foods#index'
 
   resources :recipe_foods
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :foods, only: [:index, :new, :create, :destroy]
+  resources :public_recipes, only: [:index]
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
