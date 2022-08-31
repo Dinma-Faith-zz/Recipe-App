ac03de2667f9384c549525da087d055c65a3bc7e
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-31 19:14:30 +0100

    Fix linter error

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,7 +11,7 @@ class RecipesController < ApplicationController
   # GET /recipes/1 or /recipes/1.json
   def show
     redirect_if_not_signed_in unless @recipe.public
-    
+
     # @recipe_foods = @recipe.recipe_foods.includes(:food)
   end
 
