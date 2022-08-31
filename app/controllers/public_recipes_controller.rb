38bde7b492fa6aca0b6c0cb9bcc1068a8aae9a75
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-31 20:54:15 +0100

    Fixed public recipe error

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,14 +1,15 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.where(public: true).includes(:user, :foods).order(created_at: :desc).map do |public_recipe|
-      {
-        id: public_recipe.id,
-        name: public_recipe.name,
-        user: public_recipe.user.name,
-        description: public_recipe.description,
-        food_items: public_recipe.recipe_foods.count,
-        total_price: public_recipe.foods.map(&:price).sum
-      }
-    end
+    # @public_recipes = Recipe.where(public: true).includes(@user, @foods).order(created_at: :desc).map do |public_recipe|
+    #   {
+    #     id: public_recipe.id,
+    #     name: public_recipe.name,
+    #     user: public_recipe.user_id,
+    #     description: public_recipe.description,
+    #     # food_items: public_recipe.recipe_foods.count,
+    #     # total_price: public_recipe.foods.map(&:price).sum
+    #   }
+    # end
+    @public_recipes = Recipe.where(public: true).includes(@user, @foods).order(created_at: :desc)
   end
 end
